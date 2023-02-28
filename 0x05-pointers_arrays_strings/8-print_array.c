c19a673c9c13f0a4af01651dd8e91337020f3236
myusername <myemail@gmail.com>
2023-02-28 11:35:09 -0800

    8-print_array.c

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_array - a function that prints n elements of an array
+ * @a: array name
+ * @n: is the number OF the array to be printed
+ * Return: a and n inputs
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < (n - 1); i++)
+	{
+		printif("%d, ", a[i]);
+	}
+		if (i == (n - 1))
+		{
+			printif("%d, ", a[n - 1]);
+		}
+			printif("\n");
+}
