10c2529b7119d4ccdacf901368e07652c62f944e
myusername <myemail@gmail.com>
2023-03-02 12:18:12 -0800

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -12,23 +12,22 @@ char *cap_string(char *)
 	{
 		while (!(str[index] >= 'a' && str[index] <= 'z')
 				index++;
-		if (str[index - 1] == ' ' ||
-		    str[index - 1] == '\t' ||
-		    str[index - 1] == '\n' ||
-		    str[index - 1] == ',' ||
-		    str[index - 1] == ';' ||
-		    str[index - 1] == '.' ||
-		    str[index - 1] == '!' ||
-		    str[index - 1] == '?' ||
-		    str[index - 1] == '"' ||
-		    str[index - 1] == '(' ||
-		    str[index - 1] == ')' ||
-		    str[index - 1] == '{' ||
-		    str[index - 1] == '}' ||
-		    index == 0)
-		str[index] -= 32;
-
-		index++;
-	}
+				if (str[index - 1] == ' ' ||
+					str[index - 1] == '\t' ||
+					str[index - 1] == '\n' ||
+					str[index - 1] == ',' ||
+					str[index - 1] == ';' ||
+					str[index - 1] == '.' ||
+					str[index - 1] == '!' ||
+					str[index - 1] == '?' ||
+					str[index - 1] == '"' ||
+					str[index - 1] == '(' ||
+					str[index - 1] == ')' ||
+					str[index - 1] == '{' ||
+					str[index - 1] == '}' ||
+					index == 0)
+				str[index] -= 32;
+				index++;
+				}
 	return (str);
 }
