84cf671d1fb571811402986f6a9d6fb9b65db478
myusername <myemail@gmail.com>
2023-03-02 10:40:27 -0800

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -14,12 +14,12 @@ char *_strncpy(char *dest, char *src, int n)
 	j = 0;
 	while (j < n && src[j] != '\0')
 	{
-		dest[i] = src[j];
+		dest[j] = src[j];
 		j++;
 	}
 	while (j < n)
 	{
-		dest[i] = '\0';
+		dest[j] = '\0';
 		j++;
 	}
 	return (dest);
